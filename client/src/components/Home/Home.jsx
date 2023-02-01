d4a5c48b741de522ab61701e1938af0c5db78f74
Davidvn95 <davidvergaraok@gmail.com>
2023-02-01 14:40:29 -0500

    update home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Home.module.css";
 import NavBar from "../NavBar/NavBar";
 import PokemonsCards from "../PokemonsCards/PokemonsCards";
 import Paginations from "../Paginations/Paginations";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPokemons, getAllTypes } from "../../redux/actions/actions";
 
 const Home = () => {
+    const pokemons = useSelector(state=>state.pokemons)
     const render = useSelector(state=>state.render)
     const [pagina, setPagina] = useState(1);
+    const dispatch = useDispatch()
 
     const porPagina = 8;
 
     const maximo = Math.ceil(render.length / porPagina);
 
+    useEffect(() => {
+        if (!pokemons.length) {
+            dispatch(getAllPokemons())
+            dispatch(getAllTypes())
+        }
+    })
 
     return (
         <div className={styles.home}>
